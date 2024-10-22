be37173c458bb610de800218bfc01c6bb644fe3d
prof-deets <mark.dietrich@gmail.com>
2024-10-21 20:35:47 -0400

    Initial

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+int main() {
+    
+
+    return 0;
+}
+
+/* syntax examples:
+auto start = high_resolution_clock::now()
+auto end = high_resolution_clock::now()
+auto duration = duration_cast<milliseconds>(end - start)
+duration.count() references elapsed milliseconds
+*/
