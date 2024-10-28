ae9116a4d0ee92d7c04c09509da017bb49ed90dd
youngjaychoi <youngjaychoi02@gmail.com>
2024-10-27 20:56:21 -0700

    commit3

--- main.cpp
+++ main.cpp
@@ -9,22 +9,20 @@ using namespace std;
 using namespace std::chrono;
 
 template <typename Container>
-void readData(Container &container) {
-    ifstream inFile("codes.txt");
-    string line;
-    while (getline(inFile, line)) {
-        container.insert(container.end(), line);
-    }
-    file.close();
-}
-
-
+void readData(Container &container);
+template <typename Container>
+void insertMid(Container &container, const string &value);
+void insertMid(set<string> &s, const string &value);
+template <typename Container>
+void deleteMid(Container &container);
+void deleteMid(set<string> &s);
 
 int main() {
     vector<string> vector;
     list<string> list;
     set<string> set;
 
+    
 
     auto start = high_resolution_clock::now()
     auto end = high_resolution_clock::now()
@@ -35,3 +33,31 @@ int main() {
 
     return 0;
 }
+
+template <typename Container>
+void readData(Container &container) {
+    ifstream inFile("codes.txt");
+    string line;
+    while (getline(inFile, line)) {
+        container.insert(container.end(), line);
+    }
+    file.close();
+}
+
+template <typename Container>
+void insertMid(Container &container, const string &value) {
+    auto it = container.begin();
+    advance(it, container.size() / 2);
+    container.insert(it, value);
+}
+
+void insertMid(set<string> &s, const string &value) {
+    s.insert(value);
+}
+
+template <typename Container>
+void deleteMid(Container &container) {
+    auto it = container.begin();
+    advance(it, container.size() / 2);
+    container
+}    
