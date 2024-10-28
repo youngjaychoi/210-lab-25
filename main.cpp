b891f98ec02feecb486956cc4096867819773982
youngjaychoi <youngjaychoi02@gmail.com>
2024-10-27 20:45:59 -0700

    commit2

--- main.cpp
+++ main.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include <vector>
+#include <list>
+#include <set>
+#include <algorithm>
 using namespace std;
 using namespace std::chrono;
 
-int main() {
-    
+template <typename Container>
+void readData(Container &container) {
+    ifstream inFile("codes.txt");
+    string line;
+    while (getline(inFile, line)) {
+        container.insert(container.end(), line);
+    }
+    file.close();
+}
 
+
+
+int main() {
+    vector<string> vector;
+    list<string> list;
+    set<string> set;
+
+
+    auto start = high_resolution_clock::now()
+    auto end = high_resolution_clock::now()
+    auto duration = duration_cast<milliseconds>(end - start)
+    duration.count() references elapsed milliseconds
 
 
 
     return 0;
 }
-
-/* syntax examples:
-auto start = high_resolution_clock::now()
-auto end = high_resolution_clock::now()
-auto duration = duration_cast<milliseconds>(end - start)
-duration.count() references elapsed milliseconds
-*/
